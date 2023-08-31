cf142322312a8149811c6b555675e58d830fe02e
Jose Oliveros <joliverosc@bancoripley.com>
2023-08-31 12:45:07 -0400

    test1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseX } from '@ionic-native/firebase-x/ngx';
 import { Platform } from '@ionic/angular';
+import * as Smartlook from 'cordova-plugin-smartlook';
 
 @Component({
   selector: 'app-root',
@@ -23,6 +24,9 @@ export class AppComponent implements OnInit {
         this.firebaseX.onMessageReceived().subscribe(data => {
           console.log(`FCM message: ${data}`)
         });
+        Smartlook.setProjectKey({key: '9b73e83febd539d3b3e7f2cec80c1f7ff8706e75'});
+        Smartlook.setWebViewSensitivity({ isSensitive: true });
+        Smartlook.start();
       }
     });
   }
